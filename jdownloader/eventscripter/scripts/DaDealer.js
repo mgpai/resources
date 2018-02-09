b151473ebab86dacc410124278d03932b9a5d1d4
mgpai <mgpai@yahoo.com>
2018-02-09 18:45:22 +0530

    Update DaDealer.js

--- jdownloader/eventscripter/scripts/DaDealer.js
+++ jdownloader/eventscripter/scripts/DaDealer.js
@@ -1,6 +1,7 @@
 // Update when Idle
 // Trigger Required: "Interval"
 // Set interval to 60000 (60 seconds) or more
+// Related Post: https://board.jdownloader.org/showpost.php?p=417592&postcount=294
 
 if (readyForUpdate()) callAPI("update", "restartAndUpdate");
 
