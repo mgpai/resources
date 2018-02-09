4b8713d691f7b038b9983ec6a0a117c9bc8902c7
mgpai <mgpai@yahoo.com>
2018-02-09 18:11:29 +0530

    Create DaDealer.js

---
+++ jdownloader/eventscripter/scripts/DaDealer.js
@@ -0,0 +1,15 @@
+// Update when Idle
+// Trigger Required: "Interval"
+// Set interval to 60000 (60 seconds) or more
+
+if (readyForUpdate()) callAPI("update", "restartAndUpdate");
+
+// Functions
+function readyForUpdate() {
+    if (interval < 60000) return;
+    if (!callAPI("update", "isUpdateAvailable")) return;
+    if (callAPI("linkcrawler", "isCrawling")) return;
+    if (callAPI("linkgrabberv2", "isCollecting")) return;
+    if (callAPI("extraction", "getQueue").length > 0) return;
+    return true;
+}
