ca6864f308cac415a35ae241671aa3a5c724d211
mgpai <mgpai@yahoo.com>
2018-02-17 09:51:26 +0530

    Update netgearjd.js

--- jdownloader/eventscripter/scripts/netgearjd.js
+++ jdownloader/eventscripter/scripts/netgearjd.js
@@ -5,12 +5,14 @@
 
 if (name == "Disable Matching") {
     var list = "c:/downloads/list.txt"; // <- Set  path to text file
+    var comment = "Exists on list"; // <- Set text which will be used as comment in matching links
     if (getPath(list).exists()) {
         var items = readFile(list).split("\r\n");
         getAllDownloadLinks().concat(getAllCrawledLinks()).forEach(function(link) {
             var name = link.getName();
             var matching = (items.indexOf(name) > -1);
-            if (matching) link.setEnabled(false);
+            // if (matching) link.setEnabled(false);
+            if (matching) link.setComment(comment);
         });
     } else {
         alert("\"" + list + "\" not found.");
