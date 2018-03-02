e0c02532fa829f5410cfa99f98b7e936b7482e76
mgpai <mgpai@yahoo.com>
2018-03-02 21:53:46 +0530

    Update dxzdxz1.js

--- jdownloader/eventscripter/scripts/dxzdxz1.js
+++ jdownloader/eventscripter/scripts/dxzdxz1.js
@@ -1,4 +1,4 @@
-// Run external command on extracted files
+// Run external command on extracted files & delete archive links from list
 // Trigger: "Archive extraction finished"
 // Important: Enable 'Syncrhonous execution" checkbox in top panel
 // Related Post: https://board.jdownloader.org/showpost.php?p=419505&postcount=358
