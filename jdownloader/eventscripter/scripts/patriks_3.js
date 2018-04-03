34a82e945ec87fc0386dc58efb69b7a754505f91
mgpai <mgpai@yahoo.com>
2018-04-03 23:00:09 +0530

    Update patriks_3.js

--- jdownloader/eventscripter/scripts/patriks_3.js
+++ jdownloader/eventscripter/scripts/patriks_3.js
@@ -1,12 +1,20 @@
-// Rename link/file on  vk.com
+// vk.com: If file exists ski,p else rename
 // Trigger: A Download Stopped
 // Forum Post: https://board.jdownloader.org/showpost.php?p=420584&postcount=2614
 
-var newName = link.getProperty("LINKDUPEID") + " - " + link.getProperty("FINAL_FILENAME");
-var folder = link.getPackage().getDownloadFolder();
-var existsOnDisk = getPath(folder + "/" + newName).exists();
+if (link.getHost() == "vkontakte.ru") {
 
-if (!existsOnDisk) {
-    link.setName(link.getProperty("LINKDUPEID") + " - " + link.getProperty("FINAL_FILENAME"));
-    link.setComment(link.getProperty("picturedirectlink"));
+    while (link.getBytesLoaded() == 0) sleep(1000);
+
+    var newName = link.getProperty("LINKDUPEID") + " - " + link.getProperty("FINAL_FILENAME");
+    var folder = link.getPackage().getDownloadFolder();
+    var existsOnDisk = getPath(folder + "/" + newName).exists();
+
+    if (existsOnDisk) {
+        link.reset();
+        link.remove();
+    } else {
+        link.setName(link.getProperty("LINKDUPEID") + " - " + link.getProperty("FINAL_FILENAME"));
+        link.setComment(link.getProperty("picturedirectlink"));
+    }
 }
