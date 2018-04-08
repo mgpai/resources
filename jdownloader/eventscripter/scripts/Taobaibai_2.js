cfbad09968cd37147c0c5c7e526a0d930aaddfca
mgpai <mgpai@yahoo.com>
2018-04-08 22:39:51 +0530

    Update Taobaibai_2.js

--- jdownloader/eventscripter/scripts/Taobaibai_2.js
+++ jdownloader/eventscripter/scripts/Taobaibai_2.js
@@ -1,19 +1,23 @@
-// Extract archives after all download have completed
+// Extract archives after all packages have finished
 // Trigger Required: Package Finished
 // In "Archive Extractor" settings, disable the "Extract archives after download (default value)" checkbox
 // Related Post: https://board.jdownloader.org/showpost.php?p=418712&postcount=345
 
-if (allFinished()) getAllDownloadLinks().forEach(function(link) {
-    if (!link.getArchive()) return;
-    if (!link.isFinished()) return;
-    if (link.getExtractionStatus() != null) return;
-    if (link.getArchive().getInfo().autoExtract == false) return;
-    callAPI("extraction", "startExtractionNow", [link.getUUID()], []);
-});
+if (allPackagesFinished()) startExtraction();
 
-// Function
-function allFinished() {
-    return getAllDownloadLinks().every(function(link) {
-        return link.getStatus() != null;
+// Functions
+function allPackagesFinished() {
+    return getAllFilePackages().every(function(package) {
+        return package.isFinished();
+    });
+}
+
+function startExtraction() {
+    getAllDownloadLinks().forEach(function(link) {
+        if (!link.getArchive()) return;
+        if (!link.isFinished()) return;
+        if (link.getExtractionStatus() != null) return;
+        if (link.getArchive().getInfo().autoExtract == false) return;
+        callAPI("extraction", "startExtractionNow", [link.getUUID()], []);
     });
 }
