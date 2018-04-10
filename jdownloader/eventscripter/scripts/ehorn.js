998e5b2d77a781d8ded4ca9f86cd765bb826eb8e
mgpai <mgpai@yahoo.com>
2018-04-10 16:58:00 +0530

    Create ehorn.js

---
+++ jdownloader/eventscripter/scripts/ehorn.js
@@ -0,0 +1,20 @@
+// Flatten Archives (Move extracted files from sub-folders to the main extraction folder)
+// Trigger Required: "Archive Extraction Finished"
+// Important: Enable "Sychronous Execution" checkbox in the top panel
+// Forum Topic: https://board.jdownloader.org/showthread.php?t=77296
+
+(function() {
+    archive.getExtractedFilePaths().forEach(function(path, index) {
+        if (!isSynchronous()) return;
+        var extractionFolder = archive.getExtractToFolder();
+        if (path.getParent() == extractionFolder) return;
+        var fileName = path.getName();
+        var exists = getPath(extractionFolder + "/" + fileName).exists();
+        if (exists) {
+            path.renameTo(extractionFolder + "/[" + index + "] " + fileName);
+        } else {
+            path.moveTo(extractionFolder);
+        }
+        if (!path.getParent().length) path.getParent().delete();
+    });
+})();
