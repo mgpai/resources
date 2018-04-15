02f4efdad75248a40400660784872b8e0fe671bf
mgpai <mgpai@yahoo.com>
2018-04-15 10:54:14 +0530

    Update FlyAway.js

--- jdownloader/eventscripter/scripts/FlyAway.js
+++ jdownloader/eventscripter/scripts/FlyAway.js
@@ -1,15 +1,17 @@
  // Reconnect if all downloads have been running for at least one minute, and the average (global) download speed is below user specified speed
  // Target Required: "Interval" (Set interval to 30000 or more)
- // Forum Post: https://board.jdownloader.org/showpost.php?p=422375&postcount=416
+ // Important: Enable "Synchronous Execution" checkbox in top panel
+ // Forum Topic: https://board.jdownloader.org/showthread.php?t=77331
 
- var minSpeed = 400; // <- Set minium speed in KiB/s
+ var minSpeed = 400; // <- Set minimum speed in KiB/s
 
  (function() {
      if (interval < 30000) return;
+     if (!isSynchronous) return;
      if (!isDownloadControllerRunning()) return;
      if (getAverageSpeed() > minSpeed * 1024) return;
-     if (getRunningDownloadLinks().some(function(link) {
-             return link.getDownloadDuration() < 60000;
+     if (!getRunningDownloadLinks().every(function(link) {
+             return link.getDownloadDuration() > 60000;
          })) return;
-     requestReconnect();
+     doReconnect();
  })();
