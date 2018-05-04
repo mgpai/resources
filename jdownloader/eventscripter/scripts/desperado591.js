30dac14c76caab678bb810b61e264b7cb337a06d
mgpai <mgpai@yahoo.com>
2018-05-04 22:36:51 +0530

    Update desperado591.js

--- jdownloader/eventscripter/scripts/desperado591.js
+++ jdownloader/eventscripter/scripts/desperado591.js
@@ -1,11 +1,13 @@
 // Send email notification (NAS)
-// Trigger Required: Package Finished
+// Trigger Required: A Download Stopped
 // Important: Enable "Synchronous execcution" checkbox in top panel
 
-var absender = "nas@domain.de";
-var empfaenger = "empfaenger@domain.de";
-var inhalt = getPath(JD_HOME + "/package - inhalt.txt");
+if (link.isFinished()) {
+    var absender = "nas@domain.de";
+    var empfaenger = "empfaenger@domain.de";
+    var inhalt = getPath(JD_HOME + "/link - inhalt.txt");
 
-writeFile(inhalt, "Package Finished: " + package.getName(), true);
-callSync("sendmail", "-F", absender, "-t", empfaenger, "<", inhalt);
-deleteFile(inhalt, false);
+    writeFile(inhalt, "Link Finished: " + link.getName(), true);
+    callSync("sendmail", "-F", absender, "-t", empfaenger, "<", inhalt);
+    deleteFile(inhalt, false);
+}
