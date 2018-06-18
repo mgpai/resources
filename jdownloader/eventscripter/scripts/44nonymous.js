d23349d77c4883b18017e718d715b2aaa1de73c3
mgpai <mgpai@yahoo.com>
2018-06-18 10:30:01 +0530

    Create 44nonymous.js

---
+++ jdownloader/eventscripter/scripts/44nonymous.js
@@ -0,0 +1,9 @@
+// Run external program when all packages finished
+// Trigger: Package Finished
+// Forum Post: https://board.jdownloader.org/showpost.php?p=425864&postcount=473
+
+var allPackagesFinished = getAllFilePackages().every(function(package) {
+    return package.isFinished();
+});
+
+if (allPackagesFinished) callAsync(null, "C:/Programs/P2P/uTorrent/utorrent.exe");
