f576dcaa54b04d7ecb70549dad8949cc11b172da
mgpai <mgpai@yahoo.com>
2018-06-28 23:51:08 +0530

    Create guardao_2.js

---
+++ jdownloader/eventscripter/scripts/guardao_2.js
@@ -0,0 +1,9 @@
+// Open url in browser
+// Trigger: Downloadlist Contextmenu Button Pressed
+// Customize download list menu > Add new Eventscripter trigger button > Rename it to "Oper URL" (without quotes)
+
+if (name == "Open URL") {
+    var link = dlSelection.getContextLink();
+    var url = link.getOriginURL() || link.getReferrerURL() || link.getContainerURL() || link.getContentURL();
+    openURL(url);
+}
