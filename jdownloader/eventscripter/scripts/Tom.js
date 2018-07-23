028a46ff718e645a0d1bd0184a92e8d1e1cb00bb
mgpai <mgpai@yahoo.com>
2018-07-23 20:41:38 +0530

    Update Tom.js

--- jdownloader/eventscripter/scripts/Tom.js
+++ jdownloader/eventscripter/scripts/Tom.js
@@ -1,4 +1,4 @@
-// Rebuild video stream converting dts to ac3
+// Convert dts to ac3 audio and create new video file
 // Trigger Required: A new file has been created
 // Forum Topic: https://board.jdownloader.org/showpost.php?p=427834&postcount=489
 
@@ -6,6 +6,6 @@ var ffmpeg = callAPI("config", "get", "org.jdownloader.controlling.ffmpeg.FFmpeg
 var ffprobe = callAPI("config", "get", "org.jdownloader.controlling.ffmpeg.FFmpegSetup", null, "binarypathprobe");
 
 files.forEach(function(file) {
-    if (!callSync(ffprobe, "-i", file).indexOf("Audio: dts") > -1) return;
-    callSync(ffmpeg, "-i", file, "-c:v", "copy", "-c:a", "ac3", file.replace(".mkv", "-ac3.mkv"));
+    if (callSync(ffprobe, "-i", file).indexOf("Audio: dts") == -1) return;
+    callSync(ffmpeg, "-i", file, "-map", "0", "-vcodec", "copy", "-scodec", "copy", "-acodec", "ac3", "-b:a", "640k", file.replace(".mkv", "-ac3.mkv"));
 });
