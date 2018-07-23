8ef821f1761a48a381789025aae4fbdb7658ebe1
mgpai <mgpai@yahoo.com>
2018-07-23 20:02:50 +0530

    Update Tom.js

--- jdownloader/eventscripter/scripts/Tom.js
+++ jdownloader/eventscripter/scripts/Tom.js
@@ -6,6 +6,6 @@ var ffmpeg = callAPI("config", "get", "org.jdownloader.controlling.ffmpeg.FFmpeg
 var ffprobe = callAPI("config", "get", "org.jdownloader.controlling.ffmpeg.FFmpegSetup", null, "binarypathprobe");
 
 files.forEach(function(file) {
-    if (!callSync(ffprobe, "-i", file).indexOf("Audio: dts") > -1);
+    if (!callSync(ffprobe, "-i", file).indexOf("Audio: dts") > -1) return;
     callSync(ffmpeg, "-i", file, "-c:v", "copy", "-c:a", "ac3", file.replace(".mkv", "-ac3.mkv"));
 });
