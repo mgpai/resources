e37639a64753e3f1ef1a47743e5e5fcd3f25a416
mgpai <mgpai@yahoo.com>
2018-10-29 10:31:29 +0530

    Add files via upload

--- jdownloader/eventscripter/scripts/dpinbsp.js
+++ jdownloader/eventscripter/scripts/dpinbsp.js
@@ -4,8 +4,10 @@
 // Enable 'Synchronous Execution' checkbox in top panel
 // Related Post: https: //board.jdownloader.org/showpost.php?p=432828&postcount=522
 
-getAllDownloadLinks().forEach(function(link) {
+(function() {
     if (!isSynchronous()) return;
     if (interval < 60 * 60 * 1000) return;
-    if (link.getSkippedReason() == "CAPTCHA") link.setSkipped(false);
-})
+    getAllDownloadLinks().forEach(function(link) {
+        if (link.getSkippedReason() == "CAPTCHA") link.setSkipped(false);
+    })
+})();
